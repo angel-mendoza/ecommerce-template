c0f7d14fa1207476e2b4bad11d9fb754752e8138
Angel Mendoza <mendozaangel003@gmail.com>
2022-04-24 22:06:08 -0500

    create company.js

--- src/components/Sidebars/SidebarCart.jsx
+++ src/components/Sidebars/SidebarCart.jsx
@@ -8,6 +8,8 @@ import CardDetailResume from '@/components/cards/CardDetailResume.jsx'
 import CardDetailResumeEmpty from '@/components/cards/CardDetailResumeEmpty.jsx'
 import CardTotal from '@/components/cards/CardTotal.jsx'
 
+import Company from '@/data/company'
+
 import '@/styles/SidebaCart.scss'
 
 const SidebaCart = (props) => {
@@ -38,7 +40,7 @@ const SidebaCart = (props) => {
           <>
             <CardTotal total={state.amount} />
             <div className="d-grid gap-2 mt-4">
-              <a className='btn btn-success' href={`https://wa.me/+593983211971/?text=${state.dataWts}`}>
+              <a className='btn btn-success' href={`https://wa.me/${Company.PHONE_NUMBER}/?text=${state.dataWts}`}>
                 <MdOutlinePayments size="2rem" /> Process the Payment
               </a>
             </div>
---
+++ src/data/company.js
@@ -0,0 +1,3 @@
+export default {
+  PHONE_NUMBER: '+593983211971'
+}
